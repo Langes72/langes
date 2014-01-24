fc3bdae133debf111a347512deab2aae45867246
Langes72 <langes72@gmail.com>
2014-01-24 08:38:21 -0800

    Use O3 optimization

--- build-all.sh
+++ build-all.sh
@@ -4,17 +4,17 @@
 # that calls the PAC-rom build script with the said arguments one-by-one
 
 #devices to build
-device+=(anzu_ci)
-device+=(coconut_i)
-device+=(haida_i)
-device+=(hallon_i)
-device+=(iyokan_ci)
-device+=(satsuma_i)
-device+=(smultron_i)
-device+=(urushi_i)
+device+=(anzu_fcio3)
+device+=(coconut_io3)
+device+=(haida_io3)
+device+=(hallon_io3)
+device+=(iyokan_cio3)
+device+=(satsuma_io3)
+device+=(smultron_io3)
+device+=(urushi_io3)
 
 #Setup log file and log terminal output
 t1=$(date +%d-%m-%y_%R)
 exec >> "logs/${t1}.log" #2>&1
 
-./build.py -fv ${device[@]}
+./build.py -v ${device[@]}
