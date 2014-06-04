fe159707732ddf648c5b754d6e3be20c21fde940
Langes <langes72@gmail.com>
2014-06-04 15:32:23 +0100

    do only a partial clean

--- build-all.sh
+++ build-all.sh
@@ -4,14 +4,14 @@
 # that calls the PAC-rom build script with the said arguments one-by-one
 
 #devices to build
-device+=(anzu_c1)
-device+=(coconut_c1)
-device+=(haida_c1)
-device+=(hallon_c1)
-device+=(iyokan_c1)
-device+=(mango_c1)
-device+=(satsuma_c1)
-device+=(smultron_c1)
-device+=(urushi_c1)
+device+=(anzu_c3)
+device+=(coconut_c3)
+device+=(haida_c3)
+device+=(hallon_c3)
+device+=(iyokan_c3)
+device+=(mango_c3)
+device+=(satsuma_c3)
+device+=(smultron_c3)
+device+=(urushi_c3)
 
 ./langes/build.py -v ${device[@]}
